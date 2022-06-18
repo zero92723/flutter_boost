26c7cfcd54f0b606508b6db376bbddd552ec66f6
chenli <chenli@starschina.com>
2022-06-18 08:39:43 +0800

    适配30 阿萨大

--- android/src/main/java/com/idlefish/flutterboost/containers/FlutterBoostActivity.java
+++ android/src/main/java/com/idlefish/flutterboost/containers/FlutterBoostActivity.java
@@ -156,7 +156,7 @@ public class FlutterBoostActivity extends FlutterActivity implements FlutterView
     private void performAttach() {
         if (!isAttached) {
             // Attach plugins to the activity.
-            getFlutterEngine().getActivityControlSurface().attachToActivity(getActivity(), getLifecycle());
+            getFlutterEngine().getActivityControlSurface().attachToActivity(getExclusiveAppComponent(), getLifecycle());
 
             if (platformPlugin == null) {
                 platformPlugin = new PlatformPlugin(getActivity(), getFlutterEngine().getPlatformChannel());
